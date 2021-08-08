de9edf3e2e76c9e6f2d1e74619c378118db0a54d
Carlos Salazar <carlos.salazar@orderbird.com>
2021-08-09 00:53:25 +0200

    Refactor index.js
    - Replace switch with object
    - Add aliases for functions
    - Remove unnecessary variables

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
+// Transform clipboard content. Eg:
+// node . trim double singleQuote
+
 const clipboardy = require('clipboardy')
 
-const clipboardContent = clipboardy.readSync()
+const getClipboard = () => clipboardy.readSync()
 
-const trimContent = () => clipboardContent.trim()
-const singleQuoteContent = () => `'${clipboardContent}'`
-const doubleQuoteContent = () => `"${clipboardContent}"`
-
-let res
-
-switch (process.argv.splice(2)[0]) {
-  case 'trim':
-    res = trimContent()
-    break
-  case 'singlequote':
-    res = singleQuoteContent()
-    break
-  case 'doublequote':
-    res = doubleQuoteContent()
-    break
-  default:
+// TODO: add moar functions: swap quotes, split, join, etc
+const functions = {
+  doubleQuote: () => `"${getClipboard()}"`,
+  singleQuote: () => `'${getClipboard()}'`,
+  trim: () => getClipboard().trim(),
 }
 
-clipboardy.writeSync(res)
+// TODO: FIX: this is bad and I feel bad about it
+const aliases = {
+  d: functions.doubleQuote,
+  double: functions.doubleQuote,
+  doublequote: functions.doubleQuote,
+  s: functions.singleQuote,
+  single: functions.singleQuote,
+  singlequote: functions.singleQuote,
+  t: functions.trim,
+  trim: functions.trim,
+}
+
+const arguments = process.argv.splice(2)
+
+arguments.forEach((arg) => {
+  const action = functions[arg]
+    || functions[arg.toLowerCase()]
+    || aliases[arg]
+    || aliases[arg.toLowerCase()]
+
+  if (action) {
+    console.log('action:', action)
+    clipboardy.writeSync(action())
+  }
+})
