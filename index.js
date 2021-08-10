03075fd2b2784246676c3c2a85c982aac6d37ded
Carlos Salazar <carlos.salazar@orderbird.com>
2021-08-10 19:35:38 +0200

    Refactor main entrypoint
    - Add code comments
    - Set aliases as arrays and iterate them
    - Add function to set the clipboard content
    - Add help text
    - Add verbose output to confirm actions applied

--- index.js
+++ index.js
@@ -1,39 +1,73 @@
 // Transform clipboard content. Eg:
-// node . trim double singleQuote
+// $ clipbread trim double singleQuote
+// $ clipbread t d s
 
 const clipboardy = require('clipboardy')
+const appName = 'clipbread'
 
+// Clipboard management
 const getClipboard = () => clipboardy.readSync()
+const setClipboard = (value) => clipboardy.writeSync(value)
 
-// TODO: add moar functions: swap quotes, split, join, etc
+// Function names, passed as arguments
 const functions = {
   doubleQuote: () => `"${getClipboard()}"`,
+  removeNewline: () => getClipboard().replace(/(\r|\n)/gm, ' '),
   singleQuote: () => `'${getClipboard()}'`,
   trim: () => getClipboard().trim(),
 }
 
-// TODO: FIX: this is bad and I feel bad about it
+// Aliases for the available functions
 const aliases = {
-  d: functions.doubleQuote,
-  double: functions.doubleQuote,
-  doublequote: functions.doubleQuote,
-  s: functions.singleQuote,
-  single: functions.singleQuote,
-  singlequote: functions.singleQuote,
-  t: functions.trim,
-  trim: functions.trim,
+  doubleQuote: ['adddoublequote', 'd', 'double', 'doublequote'],
+  removeNewline: ['removenl', 'rnl'],
+  singleQuote: ['addsinglequote', 's', 'single', 'singlequote'],
+  trim: ['t'],
 }
 
-const arguments = process.argv.splice(2)
+// Find function by name or alias
+const findFunctionName = (needle) => {
+  let functionName = functions[needle]
 
-arguments.forEach((arg) => {
-  const action = functions[arg]
-    || functions[arg.toLowerCase()]
-    || aliases[arg]
-    || aliases[arg.toLowerCase()]
-
-  if (action) {
-    console.log('action:', action)
-    clipboardy.writeSync(action())
+  if (! functionName) {
+    for (const [key, value] of Object.entries(aliases)) {
+      if (value.includes(needle.toLowerCase())) {
+        functionName = functions[key]
+        break
+      }
+    }
   }
+
+  return functionName || null
+}
+
+// List valid arguments and their aliases
+// The empty line is a unicode char: '‎'
+const showHelp = () => {
+  console.error(`You need to pass one or more arguments, or their alias:
+  ‎
+  ${Object.keys(functions).map(
+    (fn) => `${fn} - ${aliases[fn].join(', ')}`
+  ).join('\n')}
+  ‎
+  Example: ${appName} t double singleQuote`.replace(/^\s+/gm, ''))
+}
+
+const args = process.argv.splice(2)
+
+// Show help and exit
+if (! args.length) return showHelp()
+
+args.forEach((arg) => {
+  const functionName = findFunctionName(arg)
+
+  if (! functionName) {
+    console.error(`${arg} is an invalid argument`)
+    return
+  }
+
+  setClipboard(functionName())
+  const { name } = functionName
+  const applied = arg === name ? arg : `${arg} (${name})`
+  console.log(`${applied} applied`)
 })
