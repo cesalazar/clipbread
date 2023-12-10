a38de373aaca29feac3a51da79b9074a67214512
Carlos E. Salazar <ce.salazar@gmail.com>
2023-12-10 16:31:34 +0100

    Add multiple functions and aliases

--- index.js
+++ index.js
@@ -13,11 +13,21 @@ const setClipboard = (value) => clipboardy.writeSync(value)
 // Function names, passed as arguments
 // TODO: Include the user's functions (Eg. ~/.clipbread/config.js)
 const functions = {
+  curlyQuote: () => '`' + getClipboard() + '`',
   doubleInsideSingle: () => [functions.doubleQuote, functions.singleQuote],
   doubleQuote: () => `"${getClipboard()}"`,
+  markdownLinkFromTitle: () => `[${getClipboard()}](URL)`,
+  markdownLinkFromURL: () => `[TITLE](${getClipboard()})`,
   removeNewline: () => getClipboard().replace(/(\r|\n)/gm, ' '),
+  replaceSpaceWithDash: () => getClipboard().replace(/\s+/gm, '-'),
   singleInsideDouble: () => [functions.singleQuote, functions.doubleQuote],
   singleQuote: () => `'${getClipboard()}'`,
+  snakeToCamelCase: () =>
+    getClipboard()
+      .toLowerCase()
+      .replace(/_./g, (match) => match.charAt(1).toUpperCase()),
+  toLowerCase: () => getClipboard().toLowerCase(),
+  toUpperCase: () => getClipboard().toUpperCase(),
   trim: () => getClipboard().trim(),
   trimEachLine: () =>
     getClipboard()
@@ -29,11 +39,17 @@ const functions = {
 // Aliases for the available functions
 // TODO: Include the user's aliases (Eg. ~/.clipbread/config.js)
 const aliases = {
+  curlyQuote: ['curly', 'cq', 'curlyquote'],
   doubleInsideSingle: ['dis', 'doubleinsidesingle'],
   doubleQuote: ['adddoublequote', 'd', 'double', 'doublequote'],
+  markdownLinkFromTitle: ['mdlt'],
+  markdownLinkFromURL: ['mdlu'],
   removeNewline: ['removenl', 'rnl'],
   singleInsideDouble: ['sid', 'singleinsidedouble'],
   singleQuote: ['addsinglequote', 's', 'single', 'singlequote'],
+  snakeToCamelCase: ['snaketocamel', 'stc'],
+  toLowerCase: ['tl', 'tlc', 'lc'],
+  toUpperCase: ['tu', 'tuc', 'uc'],
   trim: ['t'],
   trimEachLine: ['te', 'trimeach', 'tpl', 'trimperline'],
 }
